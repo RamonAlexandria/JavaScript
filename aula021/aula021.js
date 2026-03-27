51f6a2e6775ad2b59beeaec971ddcd5690b3294b
Ramon Alexandria <ramonalexandria.oficial@gmail.com>
2026-03-27 20:30:36 -0300

    aula022
    Aula sobre funções e parametros.

--- aula021/aula021.js
+++ aula021/aula021.js
@@ -1,8 +1,28 @@
 "use strict"
+const valorPadrao = 0
 
-function oi(nome){
-     console.log(nome)
-    return
+function soma(n1 = valorPadrao, n2 = valorPadrao){
+    let resu = n1 * n2;
+    return resu;
+}
+let resultado = soma(10,5);
+
+console.log(resultado);
+
+//-----------------------------------------------------------------------------
+
+let valor = 0
+
+console.log(valor)
+
+function add(v){
+    return valor+v
 }
 
-oi("olá mundo")
+valor = add(10)
+
+console.log(valor)
+
+valor = add(5)
+
+console.log(valor)
